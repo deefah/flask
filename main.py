65e2c288ea4c03ca1da77901f19e925b5911fa8b
deefah <110393107+deefah@users.noreply.github.com>
2023-02-02 18:59:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from api import driver
+from configuration import route as rou
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+
+@app.route(rou.route_insert, methods=[rou.method])
+def insert_the_driver(): return driver.insert_the_driver()
+
+
+@app.route(rou.route_select, methods=[rou.method])
+def select_the_driver(): return driver.select_the_driver()
+
+@app.route(rou.route_delete, methods=[rou.method])
+def delete_the_driver(): return driver.delete_the_driver()
+
+
+@app.route(rou.route_update, methods=[rou.method])
+def update_the_driver(): return driver.update_the_driver()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host=rou.host, port=2544)
